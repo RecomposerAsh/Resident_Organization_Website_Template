212c3c9016d4578ec48f6f8f0f9e3bcd710dd003
Shaneen <shaneencotterell@gmail.com>
2022-10-30 18:28:39 -0400

    add about.js

--- JS/about.js
+++ JS/about.js
@@ -1,1 +1,127 @@
-document.createElement("h1")
+let aboutPage = document.createElement("section")
+aboutPage.innerHTML = `<section class="box">
+<div id="box-text">
+    <h1>WHAT WE ARE</h1>
+</div>
+</section>
+
+<section class="box1">
+<div id="box1-text">
+<P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora aliquam officiis error mollitia fugit quae accusamus eligendi corporis quas soluta nam doloribus 
+    facere hic velit aliquid neque rerum vel, fugiat quo repudiandae nostrum eveniet ipsam dicta! Provident, sunt rem modi corrupti nobis porro eum perferendis ut illum 
+    debitis, neque saepe quibusdam nostrum nisi nesciunt asperiores aperiam laudantium qui? Adipisci eveniet voluptate aspernatur reprehenderit veniam dolorem reiciendis 
+    velit rerum? Atque nam facilis consequatur at possimus quibusdam. Modi, officiis pariatur numquam est, at saepe distinctio, fuga magni explicabo dicta porro ullam accusantium. 
+    Autem quia ratione dicta voluptates tempore. Consequatur, omnis harum!</P>
+</div>
+<img id="box1-image" src="https://freesvg.org/img/abstract-user-flat-4.png" alt="user pic">
+</section>
+
+<section class="box2">
+<div id="box2-text">
+<h1>WHY WE DO THIS</h1>
+</div>
+</section>
+
+<section class="box3">
+<img id="box3-image" src="https://freesvg.org/img/abstract-user-flat-4.png" alt="user pic">
+<div id="box3-text">
+<P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora aliquam officiis error mollitia fugit quae accusamus eligendi corporis quas soluta nam doloribus 
+    facere hic velit aliquid neque rerum vel, fugiat quo repudiandae nostrum eveniet ipsam dicta! Provident, sunt rem modi corrupti nobis porro eum perferendis ut illum 
+    debitis, neque saepe quibusdam nostrum nisi nesciunt asperiores aperiam laudantium qui? Adipisci eveniet voluptate aspernatur reprehenderit veniam dolorem reiciendis 
+    velit rerum? Atque nam facilis consequatur at possimus quibusdam. Modi, officiis pariatur numquam est, at saepe distinctio, fuga magni explicabo dicta porro ullam accusantium. 
+    Autem quia ratione dicta voluptates tempore. Consequatur, omnis harum!</P>
+</div>
+</section>
+
+<section class="box4">
+<div id="box4-text">
+<h1>CREATOR STORIES</h1>
+</div>
+</section>
+
+<section id="box5">
+<div id="box5-text">
+<h2>ASH</h2>
+<P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora aliquam officiis error mollitia fugit quae accusamus eligendi corporis quas soluta nam doloribus 
+    facere hic velit aliquid neque rerum vel, fugiat quo repudiandae nostrum eveniet ipsam dicta! Provident, sunt rem modi corrupti nobis porro eum perferendis ut illum 
+    debitis, neque saepe quibusdam nostrum nisi nesciunt asperiores aperiam laudantium qui? Adipisci eveniet voluptate aspernatur reprehenderit veniam dolorem reiciendis 
+    velit rerum? Atque nam facilis consequatur at possimus quibusdam. Modi, officiis pariatur numquam est, at saepe distinctio, fuga magni explicabo dicta porro ullam accusantium. 
+    Autem quia ratione dicta voluptates tempore. Consequatur, omnis harum!</P>
+</div>
+</section>
+
+<section id="box6">
+<div id="box6-text">
+<h2>KY</h2>
+<P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora aliquam officiis error mollitia fugit quae accusamus eligendi corporis quas soluta nam doloribus 
+    facere hic velit aliquid neque rerum vel, fugiat quo repudiandae nostrum eveniet ipsam dicta! Provident, sunt rem modi corrupti nobis porro eum perferendis ut illum 
+    debitis, neque saepe quibusdam nostrum nisi nesciunt asperiores aperiam laudantium qui? Adipisci eveniet voluptate aspernatur reprehenderit veniam dolorem reiciendis 
+    velit rerum? Atque nam facilis consequatur at possimus quibusdam. Modi, officiis pariatur numquam est, at saepe distinctio, fuga magni explicabo dicta porro ullam accusantium. 
+    Autem quia ratione dicta voluptates tempore. Consequatur, omnis harum!</P>
+</div>
+</section>
+
+<section id="box7">
+<div id="box7-text">
+<h2>SHANEEN</h2>
+<P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora aliquam officiis error mollitia fugit quae accusamus eligendi corporis quas soluta nam doloribus 
+    facere hic velit aliquid neque rerum vel, fugiat quo repudiandae nostrum eveniet ipsam dicta! Provident, sunt rem modi corrupti nobis porro eum perferendis ut illum 
+    debitis, neque saepe quibusdam nostrum nisi nesciunt asperiores aperiam laudantium qui? Adipisci eveniet voluptate aspernatur reprehenderit veniam dolorem reiciendis 
+    velit rerum? Atque nam facilis consequatur at possimus quibusdam. Modi, officiis pariatur numquam est, at saepe distinctio, fuga magni explicabo dicta porro ullam accusantium. 
+    Autem quia ratione dicta voluptates tempore. Consequatur, omnis harum!</P>
+</div>
+</section>`
+
+document.body.appendChild(aboutPage)
+
+
+
+// let container1 = document.createElement("div")
+// let title = document.createElement("h1");
+// title.innerText = ("WHAT WE ARE")
+// container1.appendChild(title)
+
+// container1.style.width="80%";
+// container1.style.height="500px";
+// container1.style.textAlign="center";
+// container1.style.position="relative";
+// container1.style.marginTop="2vh";
+// container1.style.marginLeft="10%";
+
+
+
+// let para1 = document.createElement("p")
+// para1.innerText = ("Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt fugiat hic incidunt itaque minima officia, exercitationem dolores dolor dignissimos ab harum esse iure suscipit veritatis explicabo dicta illo, architecto consequatur sed optio mollitia quo aspernatur? Odit soluta voluptas laudantium eos! Cumque corrupti excepturi laborum cum rem ab necessitatibus odit sapiente exercitationem? Nemo repellendus error, libero animi aperiam laboriosam dolorum soluta! Non nemo consequuntur sequi quas id architecto dolore iusto eaque!")
+// container1.appendChild(para1)
+
+
+// container1.style.display="flex";
+// container1.style.flexDirection="column";
+
+
+// const centerDiv = () =>{
+// }
+
+// centerDiv();
+// window.addEventListener('resize',centerDiv)
+
+
+
+// let container3 = document.createElement("div")
+// document.body.appendChild(container3)
+
+
+// let container4 = document.createElement("div")
+// document.body.appendChild(container4)
+
+
+// let container5 = document.createElement("div")
+// document.body.appendChild(container5)
+
+
+// let container6 = document.createElement("div")
+// document.body.appendChild(container6)
+
+
+
+
