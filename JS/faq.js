3fd041f49b0a1e919e37866caff19b8d63f1dea3
Shaneen <shaneencotterell@gmail.com>
2022-11-01 15:36:46 -0400

    Update faq.js

--- JS/faq.js
+++ JS/faq.js
@@ -3,7 +3,10 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
     button.addEventListener('click', ()=>{
         const faq = button.nextElementSibling;
-        const icon = button.children[1]
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
     })
 
 });
