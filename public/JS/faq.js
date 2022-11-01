7b9406625d83e53a7b4f7d4c37697f71ebd759d0
Shaneen <shaneencotterell@gmail.com>
2022-11-01 12:31:23 -0400

    add faq js

---
+++ public/JS/faq.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1]
+    })
+
+});
