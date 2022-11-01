7dd173dce11631cfc2bbbda559b9bc229bd8589d
Shaneen <shaneencotterell@gmail.com>
2022-11-01 12:30:49 -0400

    Delete faq.js

--- JS/faq.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        const faq = button.nextElementSibling;
-        const icon = button.children[1]
-    })
-
-});
